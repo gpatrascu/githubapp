f71d3e650d3919cc5efb2d1e1b47520bc704602f
george patrascu <george.patrascu@yahoo.com>
2019-09-18 21:03:10 +0300

    github app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CardList = ({profiles}) =>
+    (
+        <div>
+            {profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+        </div>
+    );
+
+const Card = (profile) => {
+    return (
+        <div className="github-profile">
+            <img src={profile.avatar_url}/>
+            <div className="info">
+                <div className="name">{profile.name}</div>
+                <div className="company">{profile.company}</div>
+            </div>
+        </div>
+    )
+}
+
+const Form = ({onSubmit}) =>{
+    const [userName, setUserName] = useState('');
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        const resp = await Axios.get(`https://api.github.com/users/${userName}`);
+        await onSubmit(resp.data);
+        setUserName('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text"
+                   value = {userName}
+                   onChange={event => setUserName(event.target.value)}
+                   placeholder="GitHub username"
+                   required/>
+            <button>Add card</button>
+        </form>
+    ); 
+}
+
+const App = ({title}) => {
+
+    const [profiles, setProfiles] = useState([]);
+    
+    const addNewProfile = (profile) =>{
+        setProfiles([...profiles, profile]);
+    };
+
+    return (
+        <div>
+            <div className="header">{title}</div>
+            <Form onSubmit = {addNewProfile}/>
+            <CardList profiles = {profiles}/>
+        </div>
+    );
 }
 
 export default App;
